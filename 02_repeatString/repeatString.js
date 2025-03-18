edaaa15101dd8e82d2afaa90336969c0d5be2d48
Sodiq Ganiyu <ganiyus.z@accessbankplc.com>
2025-03-18 08:03:45 +0100

    finish the 1st assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
 
-};
+//   let num = 3;
+//   let result = "";
+//   function repeatStr(str, num) {
+//       for (let i = 0; i < num; i++){
+//           result += `${str}`;
+//       }
+//       console.log(result);
+//   }
 
 // Do not edit below this line
 module.exports = repeatString;
