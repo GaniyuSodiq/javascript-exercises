a7c3703c674af06c711d4de7f0ce1450c6f982e1
Sodiq Ganiyu <ganiyus.z@accessbankplc.com>
2025-03-18 08:27:50 +0100

    Reverse a a given string
    take in the input maybe with prompt()
    split the characters into array using split("") splitting from space
    btwn each character
    reverse the splitted characters that are in array use reverse()
    now join the reversed chars back to a string using join("")

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordReversed = word.split("").reverse().join("");
+    return wordReversed;
 };
+
+reverseString(word);
 
 // Do not edit below this line
 module.exports = reverseString;
