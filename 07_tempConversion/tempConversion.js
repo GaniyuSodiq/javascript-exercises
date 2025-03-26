3a610701c74607d6b66b78045ada77b6e259a811
Sodiq Ganiyu <ganiyus.z@accessbankplc.com>
2025-03-26 15:51:39 +0100

    this is a temp converter assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const celius = Math.round(((num - 32) * (5/9)) * 10) / 10;
+  return celius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const fahrenheit = Math.round(((num * (9/5)) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
