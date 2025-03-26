bb0f327ef332023d6b4807bfcddf17b62836545a
Sodiq Ganiyu <ganiyus.z@accessbankplc.com>
2025-03-26 09:44:56 +0100

    Make non intergers give error
    this is useful for only accepting integer number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,24 @@
 const sumAll = function(firstNum, secondNum) {
+    if (firstNum < 0 || secondNum < 0){
+        return "ERROR";
+    }
+    if ((!Number.isInteger(firstNum)) || (!Number.isInteger(secondNum))){
+        return "ERROR";
+    }
     let sum = 0;
-    for (let i = firstNum; i <= secondNum; i++){
-        sum += i;
+    if (secondNum > firstNum){
+        for (let i = firstNum; i <= secondNum; i++){
+            sum += i;
+        }
+    } else if (secondNum < firstNum) {
+        for (let i = secondNum; i <= firstNum; i++){
+            sum += i;
+        }
+    } else {
+        sum = firstNum + secondNum
     }
     return sum
+    
 };
 
 // Do not edit below this line
