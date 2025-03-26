cf2afa7c42b7f9fe6185dc4866211fcfc93612b6
Sodiq Ganiyu <ganiyus.z@accessbankplc.com>
2025-03-26 10:02:24 +0100

    How i googled the non integer part

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -21,5 +21,11 @@ const sumAll = function(firstNum, secondNum) {
     
 };
 
+// HERE IS WHAT HAPPENED
+// i know how to go about adding the numbers. I know loop will count from the least uptill the highest
+// but i didt know how to detect if the value is integer or not
+// so did the mighty o'l google and landed on this https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+// from this search "how to get non integer parameter in javascript"
+
 // Do not edit below this line
 module.exports = sumAll;
